5e2afedb260cd98bfceb7e430c74263f726d38c8
Zartab Nakhwa <zartab@codewithz.com>
2021-11-29 18:04:40 +0530

    Button Implementation

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    console.log('Button Pressed')
+  }
+
   return (
     <View style={styles.container}>
-      <Text> Hello React Native</Text>
-      <TouchableOpacity onPress={() => console.log('Image Pressed')}>
-        <Image source={{
-          uri: 'https://saib.com.sa/sites/default/files/logo.png',
-          width: 412,
-          height: 122
-
-        }} />
-
-      </TouchableOpacity>
+      <Button color="orange" title="Click Me" onPress={handlePress} />
     </View>
   );
 }
