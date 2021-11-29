d98f5d49e0eedd777df5e10f76b83066ed76aaa3
Zartab Nakhwa <zartab@codewithz.com>
2021-11-29 17:33:14 +0530

    Touchable Component

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> Hello React Native</Text>
-      <Image source={{
-        uri: 'https://saib.com.sa/sites/default/files/logo.png',
-        width: 412,
-        height: 122
+      <TouchableOpacity onPress={() => console.log('Image Pressed')}>
+        <Image source={{
+          uri: 'https://saib.com.sa/sites/default/files/logo.png',
+          width: 412,
+          height: 122
 
-      }} />
+        }} />
+
+      </TouchableOpacity>
     </View>
   );
 }
