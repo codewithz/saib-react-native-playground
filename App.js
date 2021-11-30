5f3bb76d6a9d3246dc5efc7ec6e2a627f2c2b06e
Zartab Nakhwa <zartab@codewithz.com>
2021-11-30 12:10:34 +0530

    Implementing Flex

--- App.js
+++ App.js
@@ -15,32 +15,34 @@ import { useDimensions, useDeviceOrientation } from "@react-native-community/hoo
 
 export default function App() {
 
-  console.log(useDimensions());
-  console.log(useDeviceOrientation());
-
-  const { landscape } = useDeviceOrientation();
-
-  const handlePress = () => {
-    console.log('Button Pressed')
-  }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{ backgroundColor: '#eee', flex: 1 }} >
+
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? '100%' : '30%'
-      }}></View>
-    </SafeAreaView>
+        flex: 2,
+      }} >
+      </View>
+
+      <View style={{
+        backgroundColor: 'gold',
+        flex: 1,
+      }} >
+      </View>
+
+      <View style={{
+        backgroundColor: 'tomato',
+        flex: 1,
+      }} >
+      </View>
+
+
+    </View>
+
+
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
-
-
-  },
-});
+const styles = StyleSheet.create({});
