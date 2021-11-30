f598410b085d3f94bef40c3373c12fc223bc1790
Zartab Nakhwa <zartab@codewithz.com>
2021-11-30 12:38:12 +0530

    flexWrap and align Content

--- App.js
+++ App.js
@@ -21,16 +21,18 @@ export default function App() {
       backgroundColor: '#eee',
       flex: 1,
       flexDirection: 'row', //column,row,column-reverse,row-reverse
-      justifyContent: 'space-evenly', //main axis
+      justifyContent: 'center', //main axis
       alignItems: 'center', //secondary
+      alignContent: 'center',
+      flexWrap: "wrap",
 
     }} >
 
       <View style={{
         backgroundColor: 'dodgerblue',
         width: 100,
-        height: 300,
-        alignSelf: 'flex-start'
+        height: 100,
+
       }} >
       </View>
 
@@ -43,6 +45,30 @@ export default function App() {
 
       <View style={{
         backgroundColor: 'tomato',
+        width: 100,
+        height: 100,
+      }} >
+      </View>
+      <View style={{
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100,
+      }} >
+      </View>
+      <View style={{
+        backgroundColor: 'greenyellow',
+        width: 100,
+        height: 100,
+      }} >
+      </View>
+      <View style={{
+        backgroundColor: 'purple',
+        width: 100,
+        height: 100,
+      }} >
+      </View>
+      <View style={{
+        backgroundColor: 'orange',
         width: 100,
         height: 100,
       }} >
